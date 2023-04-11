c6d0533e16f85f58c0935d1b3f3b9aa4dd0c4a99
Ben Elferink <ben.elferink@icloud.com>
2023-04-11 13:47:48 +0300

    added TX route

---
+++ pages/api/transaction/[transaction_id]/index.ts
@@ -0,0 +1,50 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import blockfrost from '../../../../utils/blockfrost'
+import type { Transaction } from '@/@types'
+
+export interface TransactionResponse extends Transaction {}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<TransactionResponse>) => {
+  const { method, query } = req
+
+  const transactionId = query.transaction_id?.toString()
+
+  if (!transactionId) {
+    return res.status(400).end()
+  }
+
+  try {
+    switch (method) {
+      case 'GET': {
+        console.log('Fetching TX:', transactionId)
+
+        const tx = await blockfrost.txs(transactionId)
+        const block = tx.block
+
+        console.log('Fetched TX:', block)
+
+        const payload = {
+          transactionId,
+          block,
+        }
+
+        return res.status(200).json(payload)
+      }
+
+      default: {
+        res.setHeader('Allow', 'GET')
+        return res.status(405).end()
+      }
+    }
+  } catch (error: any) {
+    console.error(error)
+
+    if (error?.message === 'The requested component has not been found.') {
+      return res.status(404).end(`${error.message} ${transactionId}`)
+    }
+
+    return res.status(500).end()
+  }
+}
+
+export default handler
